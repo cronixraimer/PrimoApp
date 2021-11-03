c53d79cb83c9c1a7e1f9165dafcc4ee800e16e77
cronixraimer <ayubovbakhtiyor@gmail.com>
2021-11-03 16:11:41 +0400

    Setting up the Detail of Main Departments

--- screens/home.js
+++ screens/home.js
@@ -37,11 +37,54 @@ const Home = ({navigation}) => {
         }
     ]
 
-    return(
-        <View>
-            <Text> Home </Text>
-        </View>
-    )
+    const serviceData = [
+        {
+            id: 1,
+            categories: [1],
+            photo: images.image_1,
+            duration: "MINIMUM OF SERVICE 3 HOURS",
+
+            menu: [
+                {
+                    menuID: 1,
+                    name: "Home Cleaning",
+                    photo: images.image_1,
+                    description: "Best home cleaning in DUBAI",
+                    price: 34
+                },
+                {
+                    menuID: 2,
+                    name: "Primo Turbo Cleaning",
+                    photo: images.image_2,
+                    description: "Best home cleaning in DUBAI",
+                    price: 37
+                }
+            ]
+        }
+    ]
+
+    const [categories, setCategories] = React.useState(categoryDepartments);
+    const [selectedCategory, setSelectedCategory] = React.useState(null);
+    const [services, setServices] = React.useState(servicesData)
+   
+    function getCategoryNameById(id) {
+        let category = categories.filter(a => a.id == id)
+
+        if (category.length > 0)
+            return category[0].name
+
+        return ""
+    }
+
+    function onSelectCategory(category) {
+        let serviceList = servicesData.filter(a => a.categories.includes(category.id))
+        
+        setServices(serviceList)
+
+        setSelectedCategory(category)
+    }
+
+    
 }
 
 export default Home;
