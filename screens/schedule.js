a620d9c0d8d1d9b2c2d0d6b8b0c8912b4774d438
cronixraimer <ayubovbakhtiyor@gmail.com>
2022-03-22 16:47:11 +0400

    Broke all lib and project has to be fixed

--- screens/schedule.js
+++ screens/schedule.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native'
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
+
 
 const Schedule = () => {
     return (
